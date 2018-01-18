0d183b1b67d3e6ec156f8f4cff9e36d888400a8c
luern0313 <liupeiran0313@163.com>
2018-01-18 17:49:05 +0800

    更新 fileselectAct.java

--- LittleNotes2/app/src/main/java/com/stl/wristNotes/fileselectAct.java
+++ LittleNotes2/app/src/main/java/com/stl/wristNotes/fileselectAct.java
@@ -206,48 +206,29 @@ public class fileselectAct extends Activity
 public class Adapter extends BaseAdapter{
 
 	private List<String> mData;//定义数据。
-	private LayoutInflater mInflater;//定义Inflater,加载我们自定义的布局。
+	private LayoutInflater mInflater = R.id.menulistText;//定义Inflater,加载我们自定义的布局。
 
 	/*
     定义构造器，在Activity创建对象Adapter的时候将数据data和Inflater传入自定义的Adapter中进行处理。
     */
 	public Adapter(LayoutInflater inflater,List<String> data){
-		mInflater = inflater;
 		mData = data;
-	}
-
-	@Override
-	public int getCount() {
-		return mData.size();
-	}
-
-	@Override
-	public Object getItem(int position) {
-		return position;
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertview, ViewGroup viewGroup) {
 		//获得ListView中的view
-		View viewStudent = mInflater.inflate(R.layout.item_simpleadapter,null);
-		//获得学生对象
-		Student student = mData.get(position);
+		View layoutview = mInflater.inflate(R.layout.item_simpleadapter,null);
+
+		for(int i = 0; i < mdata.length; i++)
+		{
+
+		}
 		//获得自定义布局中每一个控件的对象。
 		ImageView imagePhoto = (ImageView) viewStudent.findViewById(R.id.image_photo);
 		TextView name = (TextView) viewStudent.findViewById(R.id.textview_name);
-		TextView age = (TextView) viewStudent.findViewById(R.id.textview_age);
-		TextView sex = (TextView) viewStudent.findViewById(R.id.textview_sex);
-		TextView hobby = (TextView) viewStudent.findViewById(R.id.textview_hobby);
 		//将数据一一添加到自定义的布局中。
 		imagePhoto.setImageResource(student.getImag());
-		name.setText(student.getName());
-		age.setText(student.getAge());
-		sex.setText(student.getSex());
 		hobby.setText(student.getHobby());
 		return viewStudent ;
 	}
