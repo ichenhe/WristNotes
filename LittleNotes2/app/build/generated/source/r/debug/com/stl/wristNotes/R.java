ed1241ac7b932b33e70ab228ab462491e6370013
luern0313 <liupeiran0313@163.com>
2018-02-24 13:18:24 +0800

    .

--- LittleNotes2/app/build/generated/source/r/debug/com/stl/wristNotes/R.java
+++ LittleNotes2/app/build/generated/source/r/debug/com/stl/wristNotes/R.java
@@ -11,7 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int gray=0x7f040000;
+        public static final int gray=0x7f050000;
     }
     public static final class drawable {
         public static final int about=0x7f020000;
@@ -126,13 +126,13 @@ public final class R {
         public static final int widget_title=0x7f030013;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int helptext=0x7f050001;
+        public static final int app_name=0x7f060000;
+        public static final int helptext=0x7f060001;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int button=0x7f060001;
-        public static final int listview=0x7f060002;
-        public static final int titleText=0x7f060003;
+        public static final int AppTheme=0x7f040000;
+        public static final int button=0x7f040001;
+        public static final int listview=0x7f040002;
+        public static final int titleText=0x7f040003;
     }
 }
